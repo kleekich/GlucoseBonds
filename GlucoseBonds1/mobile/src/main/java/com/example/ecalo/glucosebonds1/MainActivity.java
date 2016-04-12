3a7ce61ab12827899b68f7c641af722c1c6adee6
Eric Calonge <Eric Calonge>
2016-04-12 12:03:13 -0700

    a lil work

--- GlucoseBonds1/mobile/src/main/java/com/example/ecalo/glucosebonds1/MainActivity.java
+++ GlucoseBonds1/mobile/src/main/java/com/example/ecalo/glucosebonds1/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.ecalo.glucosebonds1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logo = (ImageButton) findViewById(R.id.logo_pic);
+        logo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent next = new Intent();
+                startActivity(next);
+            }
+        });
     }
+
 }
