e2856816cf42a3f67b500367a7ce7fd1eec9f195
kleekich <kleekich@berkeley.edu>
2016-05-04 12:45:14 -0700

    switching to finalServer2

--- GlucoseBonds1/mobile/src/main/java/com/example/ecalo/glucosebonds1/GlucoseBonds.java
+++ GlucoseBonds1/mobile/src/main/java/com/example/ecalo/glucosebonds1/GlucoseBonds.java
@@ -16,14 +16,19 @@ public class GlucoseBonds extends Application {
         super.onCreate();
         BaasBox.Builder b = new BaasBox.Builder(this);
         // have to use 10.0.3.2 for genymotion
-        // String apiDomain = "192.168.1.183";
+        //String apiDomain = "192.168.1.183";
         String apiDomain = "10.0.3.2";
         //String apiDomain = "10.0.2.2";
-        //String apiDomain = "192.168.1.183";
+        //String apiDomain = "192.168.57.1";
+        //String apiDomain = "192.168.56.2";
+        //String apiDomain = "10.142.16.43";
+        //String apiDomain = "192.168.138.1";
+        //String apiDomain = "10.142.16.1";
+        //String apiDomain = "50.141.29.2";
         client = b.setApiDomain(apiDomain)
                 .setPort(9000)
                 .setAppCode("1234567890")
-                .setHttpConnectionTimeout(30000)
+                .setHttpConnectionTimeout(3000)
                 .init();
     }
 
